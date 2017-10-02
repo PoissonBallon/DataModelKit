c7e0e3078faac9536f20a44a1fa8f10bb2298888
Allan Vialatte <allan@smartnsoft.com>
2017-10-02 12:10:22 +0200

    Add Open and Parse file

---
+++ Sources/DataModelKit/DataModel.swift
@@ -0,0 +1,32 @@
+//
+//  DataModel.swift
+//  DataModelKitPackageDescription
+//
+//  Created by Allan Vialatte on 02/10/2017.
+//
+
+import Foundation
+import FileKit
+import SWXMLHash
+
+struct DataModel {
+  private let path: Path
+  private let data: XMLIndexer
+  
+  init(with path:String) throws {
+    let resolvedPath = Path(path).resolved
+    let file = File<Data>.init(path: resolvedPath + "contents")
+
+    guard resolvedPath.pathExtension == DataModelKit.dataModelExtension else {
+      throw DataModelError.targetIsNotAnXcdataModel
+    }
+    guard file.exists else {
+      throw DataModelError.targetHaveNotContent
+    }
+    guard let data = try? file.read() else {
+      throw DataModelError.targetIsNotReadable
+    }
+    self.path = resolvedPath
+    self.data = SWXMLHash.parse(data)
+  }
+}
---
+++ Sources/DataModelKit/DataModelError.swift
@@ -0,0 +1,14 @@
+//
+//  DataModelError.swift
+//  DataModelKit
+//
+//  Created by Allan Vialatte on 02/10/2017.
+//
+
+import Foundation
+
+enum DataModelError: Error {
+  case targetIsNotAnXcdataModel
+  case targetHaveNotContent
+  case targetIsNotReadable
+}
