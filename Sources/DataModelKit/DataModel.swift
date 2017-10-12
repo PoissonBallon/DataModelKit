919a2abe155b76ef7e41f0678abe1d3eac694ce8
Allan Vialatte <allan.vialatte@icloud.com>
2017-10-12 23:40:44 +0200

    Add licence header and documentation

--- Sources/DataModelKit/DataModel.swift
+++ Sources/DataModelKit/DataModel.swift
@@ -1,26 +1,52 @@
 //
 //  DataModel.swift
-//  DataModelKitPackageDescription
+//  DataModelKit
 //
-//  Created by Allan Vialatte on 02/10/2017.
+//  The MIT License (MIT)
+//
+//  Copyright (c) 2017 Vialatte Allan
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
 //
 
 import Foundation
 import FileKit
 import SWXMLHash
 
+/// DataModel
 public struct DataModel {
   private let root: XMLIndexer
   
-  public let path: Path
-  public let entities: [Entity]
-  public let documentVersion: String
-  public let systemVersion: String
-  public let minimumToolsVersion: String
-  public let lastSavedToolsVersion: String
+  public let path: Path   /// Path of the original file.xcdatamodel
+  public let entities: [Entity] /// Parsed model's entities
+  public let documentVersion: String /// Version of file.xcdatamodel
+  public let systemVersion: String /// System version of file.xcdatamodel
+  public let minimumToolsVersion: String /// Minimum tools version of file.xcdatamodel
+  public let lastSavedToolsVersion: String /// Last saved tools version of file.xcdatamodel
 }
 
 extension DataModel {
+
+  /// Parse and init DataModel
+  ///
+  /// - Parameter path: Take the path of an file.xcdatamodel
+  /// - Throws: throw different error if needed
   public init(with path: String) throws {
     
     let resolvedPath = Path(path).resolved
